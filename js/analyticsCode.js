5890322ca31659d1b427bb982005361e524c0bf2
iceanfire <iceanfire@gmail.com>
2013-08-22 11:04:36 +0100

    adding some more code into analytics so it doesn't track 1.shoefind.appspot.com

--- js/analyticsCode.js
+++ js/analyticsCode.js
@@ -1,12 +1,17 @@
+//Loads google analytics code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-43377715-1', 'projectperfectshoe.com');
+//determines which analytics to run based on the domain
+if(document.URL=="http://shoefind.appspot.com/"){ga('create', 'UA-43201587-1', 'shoefind.appspot.com');}
+else{ga('create', 'UA-43377715-1', 'projectperfectshoe.com');}
+
+//sends page view to appengine
 ga('send', 'pageview');
 
-
+//array function that lets us search arrays included here for IE (windows phone) support.
 if (!Array.prototype.indexOf) {
   Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
     'use strict';
@@ -40,10 +45,15 @@ if (!Array.prototype.indexOf) {
   };
 }
 
+
+//event send function. Special function because now we have a central place to log & control how events are sent to GA
 function sendEvent(category, action, label, value){
 	productionUrl = ["http://shoefind.appspot.com/","http://shoefind.appspot.com","http://www.projectperfectshoe.com/","http://www.projectperfectshoe.com"];
 	stagingUrl = "";
-	if(productionUrl.indexOf(document.URL)>=0){
+	if(document.URL.indexOf("1.")>=0){
+		console.log('Event triggered but not sent to GA-admin');
+	}
+	else if(productionUrl.indexOf(document.URL)>=0){
 		ga('send', 'event', category, action, label, value);
 		console.log('Event Sent to GA');
 	}
@@ -51,6 +61,10 @@ function sendEvent(category, action, label, value){
 		console.log('Event triggered but not sent to GA');
 	}
 }
+
+//=================
+//Clicks to track |
+//=================
 
 $('#toggle-wishlist').click(function(){
 	sendEvent('ux', 'click', 'wishlist');
@@ -67,6 +81,3 @@ $('.ui-dialog-contain img').click(function(){
 $('.remove').on('click', function(){
 	sendEvent('ux', 'click', 'deleteItem');
 })
-
-
-
